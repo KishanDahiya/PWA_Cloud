d7a438ac10236c90546ea29ec06529cdeead32da
KishanDahiya <kkd999.km@gmail.com>
2023-02-18 22:20:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Check if service worker is supported
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => {
+        console.log('Service Worker: Registered', reg);
+      })
+      .catch((err) => console.log(`Service Worker: Error: ${err}`));
+  });
+}
